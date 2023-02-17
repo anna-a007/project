976c46455b3c0ec553a6db653e19b1a695ee222e
anna <test@mail.ru>
2023-02-17 12:39:00 +0300

    add data

--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -1,15 +1,13 @@
-import EmployeesListItem from '../employees-list-item/employees-list-item';
+import EmployeesListItem from "../employees-list-item/employees-list-item";
 
-import './employees-list.css';
+import "./employees-list.css";
 
-const EmployeesList = () => {
-  return (
-    <ul className="app-list list-group">
-      <EmployeesListItem />
-      <EmployeesListItem />
-      <EmployeesListItem />
-    </ul>
-  );
+const EmployeesList = ({ data }) => {
+  const element = data.map((item) => {
+    const { id, ...itemProps } = item; //вытягиваем из объекта item
+    return <EmployeesListItem {...itemProps} key={id} />;
+  });
+  return <ul className="app-list list-group">{element}</ul>;
 };
 
 export default EmployeesList;
