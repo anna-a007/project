f0a649c302e2c732bd05f8b9b3359cc92b31ba45
anna <test@mail.ru>
2023-02-28 16:11:02 +0300

    add delete item

--- src/components/employees-list-item/employees-list-item.jsx
+++ src/components/employees-list-item/employees-list-item.jsx
@@ -1,5 +1,4 @@
 import "./employees-list-item.css";
-import classNames from "classnames";
 import { Component } from "react";
 
 class EmployeesListItem extends Component {
@@ -28,7 +27,7 @@ class EmployeesListItem extends Component {
   };
 
   render() {
-    const { name, salary } = this.props;
+    const { name, salary, onDeleteItem } = this.props;
     const { increase, rise } = this.state;
 
     let classNames = "list-group-item d-flex justify-content-between";
@@ -58,7 +57,11 @@ class EmployeesListItem extends Component {
             <i className="fas fa-cookie"></i>
           </button>
 
-          <button type="button" className="btn-trash btn-sm ">
+          <button
+            type="button"
+            onClick={onDeleteItem}
+            className="btn-trash btn-sm "
+          >
             <i className="fas fa-trash"></i>
           </button>
           <i className="fas fa-star"></i>
--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -2,10 +2,16 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import "./employees-list.css";
 
-const EmployeesList = ({ data }) => {
+const EmployeesList = ({ data, onDeleteItem }) => {
   const element = data.map((item) => {
     // const { id, ...itemProps } = item; //вытягиваем из объекта item
-    return <EmployeesListItem {...item} key={item.id} />;
+    return (
+      <EmployeesListItem
+        {...item}
+        key={item.id}
+        onDeleteItem={() => onDeleteItem(item.id)}
+      />
+    );
   });
   return <ul className="app-list list-group">{element}</ul>;
 };
